c84f321b98d2aa7f795bbd161b01735cc38f5699
Ashton Teng <yqteng@berkeley.edu>
2015-10-03 14:03:32 -0700

    tests

---
+++ Tests.java
@@ -0,0 +1,17 @@
+package HackJam
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/** Tests
+ *  
+ */
+
+public class Tests {
+
+    @Test
+    public void testCalculateCentroid() {
+
+        //assertEquals();
+    }
+}
