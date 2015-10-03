6b8734dda2bedf0846fafa7874a24d11ea0d82cc
wharshali <harshali.wadge@gmail.com>
2015-10-03 14:05:45 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-package HackJam
+package HackJam;
+
+import java.lang.String;
+import java.lang.ArrayList;
 
 public class Main {
-	
+	public ArrayList<Person> people;
+
+	/*** 
+	pass PEOPLE into centroid. 
+	make new centroid
+	get location of centroid **/
+	//SUP LOSER
+
 }
